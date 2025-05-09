305ebf0b40dc5bd591d71b15ff8ba3b26862b667
Charl Smuts <charlsmuts12@gmail.com>
2025-05-09 12:13:35 +0200

    added headers and footers, fixed contact form submissions

--- app.js
+++ app.js
@@ -8,6 +8,8 @@ const app = express();
 const port = 3000;
 
 app.use(express.static(path.join(__dirname, "public")));
+app.use(express.urlencoded({ extended: true }));
+
 app.set("view engine", "ejs");
 app.set("views", path.join(__dirname, "views"));
 
@@ -16,3 +18,11 @@ app.use("/", pageRoutes);
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
 });
+
+app.post('/contact' , (req, res) => {
+  console.log(req.body);
+ 
+  res.redirect('/thankyou')
+})
+
+
